ad755406336817ed3e64347c7d0e6cfd403fdf36
Naman jain <54211708+Naman1509@users.noreply.github.com>
2020-04-05 22:50:07 +0530

    Add files via upload

---
+++ MIssionary_Cannibles.c
@@ -0,0 +1,59 @@
+/*Ques. 16. Design a scheduler that can schedule the processes arriving system at periodical intervals.
+Every process is assigned with a fixed time slice t milliseconds.
+If it is not able to complete its execution within the assigned time quantum, then automated timer generates an interrupt.
+The scheduler will select the next process in the queue and dispatcher dispatches the process to processor for execution.
+Compute the total time for which processes were in the queue waiting for the processor.
+Take the input for CPU burst, arrival time and time quantum from the user. */
+ 
+#include<stdio.h> 
+ 
+int main() 
+{ 
+      int i, limit, total = 0, x, counter = 0, time_quantum;
+      int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10]; 
+      float average_wait_time, average_turnaround_time;
+      printf("\nEnter Total Number of Processes:\t"); 
+      scanf("%d", &limit); 
+      x = limit; 
+      for(i = 0; i < limit; i++) 
+      {
+            printf("\nEnter Details of Process[%d]\n", i + 1);
+            printf("Arrival Time:\t");
+            scanf("%d", &arrival_time[i]);
+            printf("Burst Time:\t");
+            scanf("%d", &burst_time[i]); 
+            temp[i] = burst_time[i];
+      } 
+      printf("\nEnter Time Quantum:\t"); 
+      scanf("%d", &time_quantum); 
+      printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\n");
+      for(total = 0, i = 0; x != 0;) 
+      { 
+            if(temp[i] <= time_quantum && temp[i] > 0) 
+            { 
+                  total = total + temp[i]; 
+                  temp[i] = 0; 
+                  counter = 1; 
+            } 
+            else if(temp[i] > 0) 
+            { 
+                  temp[i] = temp[i] - time_quantum; 
+                  total = total + time_quantum; 
+            } 
+            if(temp[i] == 0 && counter == 1) 
+            { 
+                  x--; 
+                  printf("\nProcess[%d]\t\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
+                  wait_time = wait_time + total - arrival_time[i] - burst_time[i]; 
+                  turnaround_time = turnaround_time + total - arrival_time[i]; 
+                  counter = 0; 
+            } 
+       
+      } 
+      average_wait_time = wait_time * 1.0 / limit;
+      average_turnaround_time = turnaround_time * 1.0 / limit;
+      printf("\n\nTotal Waiting Time:\t%d", wait_time); 
+      printf("\n\nAverage Waiting Time:\t%f", average_wait_time); 
+      printf("\nAvg Turnaround Time:\t%f\n", average_turnaround_time); 
+      return 0; 
+}
